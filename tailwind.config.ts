ff8ba2fa9a1aa88f087de6d23666b3e3e416f894
Martin Beňa <Bena.M@seznam.cz>
2024-04-01 13:57:20 +0200

    Import fonts and set them in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      sans: "Raleway, sans-serif",
+      serif: "Playfair Display, serif",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
