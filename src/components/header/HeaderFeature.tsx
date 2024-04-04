2ce90e5b9d0cbc8ec354bbc756affc1db991abb2
Martin Beňa <Bena.M@seznam.cz>
2024-04-04 10:31:11 +0200

    Change the font family and letterspacing for notification count for wishlist and cart

--- src/components/header/HeaderFeature.tsx
+++ src/components/header/HeaderFeature.tsx
@@ -15,7 +15,7 @@ export default function HeaderFeature({ link, children }: HeaderFeatureProps) {
       >
         {children}
       </Link>
-      <span className="absolute top-0.5 left-12 whitespace-nowrap align-middle text-sm leading-none inline-block min-h-5 min-w-5 text-zinc-800 py-[2px] px-[4px] bg-amber-200 rounded-full text-center pointer-events-none">
+      <span className="absolute top-0.5 left-12 bg-amber-200 text-zinc-800 tracking-normal min-h-5 min-w-5 text-center rounded-full leading-none flex items-center justify-center pointer-events-none py-[2px] px-1 font-sans">
         0
       </span>
     </div>
