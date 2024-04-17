89066bb67816d3ce581e956f2c33d24f6d6ae34a
Martin Beňa <Bena.M@seznam.cz>
2024-04-17 20:22:01 +0200

    Add strict query to the database function, so only the items from the schemas can be added to the database

--- src/db/connectDB.ts
+++ src/db/connectDB.ts
@@ -11,10 +11,24 @@ declare const process: {
   env: ProcessEnv;
 };
 
-export default async function ConnectDB(): Promise<void> {
-  if (mongoose.connection.readyState >= 1) return;
+interface ConnectDBResponse {
+  status: number;
+  message?: string;
+}
 
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERNAME}.itm23iu.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
-  );
+export default async function ConnectDB(): Promise<void | ConnectDBResponse> {
+  mongoose.set("strictQuery", true);
+
+  if (mongoose.connection.readyState >= 1) {
+    return { status: 200, message: "Already connected to the database" };
+  }
+
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTERNAME}.itm23iu.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`
+    );
+    return { status: 200, message: "Connected to the database successfully" };
+  } catch (error: any) {
+    return { status: 500, message: "Failed to connect to the database" };
+  }
 }
