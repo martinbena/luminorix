2b78d11558f1a1f8d8cfe983380758ec9a9fb216
Martin Beňa <Bena.M@seznam.cz>
2024-05-13 11:03:19 +0200

    Create basic structure for product management page in admin section

--- src/app/(admin)/admin/products/page.tsx
+++ src/app/(admin)/admin/products/page.tsx
@@ -1,3 +1,28 @@
+import AdminActions from "@/components/admin/AdminActions";
+import ItemTitle from "@/components/data-tables/ItemTitle";
+import Table from "@/components/data-tables/Table";
+import TableBody from "@/components/data-tables/TableBody";
+import TableContainer from "@/components/data-tables/TableContainer";
+import TableHeader from "@/components/data-tables/TableHeader";
+import TableRow from "@/components/data-tables/TableRow";
+import Button from "@/components/ui/Button";
+import HeadingSecondary from "@/components/ui/HeadingSecondary";
+import paths from "@/lib/paths";
+
 export default function AdminAllProductsPage() {
-  return <div>AdminAllProductsPage</div>;
+  return (
+    <>
+      <HeadingSecondary>Manage all products</HeadingSecondary>
+      <section className="mt-12 py-8">
+        <Button type="secondary" href={paths.adminProductCreate()}>
+          Add new products
+        </Button>
+        <TableContainer>
+          <Table>
+            <TableHeader>Products</TableHeader>
+          </Table>
+        </TableContainer>
+      </section>
+    </>
+  );
 }
--- src/lib/authConfig.ts
+++ src/lib/authConfig.ts
@@ -71,7 +71,6 @@ export const authConfig: NextAuthConfig = {
       return session;
     },
   },
-  trustHost: true,
   secret: process.env.NEXTAUTH_SECRET,
   pages: { signIn: paths.login() },
 };
