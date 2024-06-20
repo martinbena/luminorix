7841bcdad29ebd1551cf7a9966b34391f18df802
Martin Beňa <Bena.M@seznam.cz>
2024-06-20 21:37:18 +0200

    Fix the link in the user's ratings

--- src/components/user/Reviews.tsx
+++ src/components/user/Reviews.tsx
@@ -5,39 +5,56 @@ import Link from "next/link";
 import { TfiNotepad } from "react-icons/tfi";
 import Stars from "../products/Stars";
 import ReviewActions from "./ReviewActions";
+import { getFirstVariantSkuBySlug } from "@/db/queries/product";
+
+interface SlugSkuPairs {
+  [slug: string]: string;
+}
 
 export default async function Reviews() {
   const session = await auth();
   const reviews = session?.user
     ? await getUserReviews(session?.user._id.toString())
     : [];
+  const slugSkuPairs: SlugSkuPairs = {};
+  for (const review of reviews) {
+    const { slug } = review;
+    const sku = await getFirstVariantSkuBySlug(slug);
+    slugSkuPairs[slug] = sku;
+  }
+
   return (
     <>
       {reviews.length > 0 ? (
         <ul className="max-w-5xl mx-auto flex flex-col gap-6">
-          {reviews.map((item) => (
-            <li key={item.title} className="flex border border-zinc-300">
-              <div className="px-6 py-3 flex-grow">
-                <h3 className="font-semibold font-sans text-2xl mb-2">
-                  <Link href={`/${item.slug}`}>{item.title}</Link>
-                </h3>
-                <div className="mb-8">
-                  <Stars rating={item.review.rating} />
+          {reviews.map((item) => {
+            return (
+              <li key={item.title} className="flex border border-zinc-300">
+                <div className="px-6 py-3 flex-grow">
+                  <h3 className="font-semibold font-sans text-2xl mb-2">
+                    <Link
+                      className="hover:underline"
+                      href={`/${item.slug}/${slugSkuPairs[item.slug]}`}
+                    >
+                      {item.title}
+                    </Link>
+                  </h3>
+                  <div className="mb-8">
+                    <Stars rating={item.review.rating} />
+                  </div>
+                  <p className="text-base">{item.review.comment}</p>
+                  <p className="text-right font-sans text-zinc-600">
+                    Rated{" "}
+                    {format(
+                      new Date(item.review.createdAt),
+                      "MMM dd yyyy, h:mm aa"
+                    )}
+                  </p>
                 </div>
-                <p className="text-base">{item.review.comment}</p>
-                <p className="text-right font-sans text-zinc-600">
-                  Rated{" "}
-                  {format(
-                    new Date(item.review.createdAt),
-                    "MMM dd yyyy, h:mm aa"
-                  )}
-                </p>
-              </div>
-              <ReviewActions
-                rating={JSON.parse(JSON.stringify(item))}               
-              />
-            </li>
-          ))}
+                <ReviewActions rating={JSON.parse(JSON.stringify(item))} />
+              </li>
+            );
+          })}
         </ul>
       ) : (
         <div className="flex flex-col items-center justify-center gap-4">
