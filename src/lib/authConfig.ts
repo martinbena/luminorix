0b98b16499f4b171b4ed91c6095ac8b1fbbbff33
Martin Beňa <Bena.M@seznam.cz>
2024-07-24 12:01:17 +0200

    Remove authorization from Google auth provider to speed up the sign in process

--- src/lib/authConfig.ts
+++ src/lib/authConfig.ts
@@ -28,13 +28,13 @@ export const authConfig: NextAuthConfig = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
+      // authorization: {
+      //   params: {
+      //     prompt: "consent",
+      //     access_type: "offline",
+      //     response_type: "code",
+      //   },
+      // },
     }),
   ],
   callbacks: {
