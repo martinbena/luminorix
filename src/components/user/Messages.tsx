e555c79e56b8d998c342f09055b7f16c3786d05a
Martin Beňa <Bena.M@seznam.cz>
2024-07-24 14:28:01 +0200

    Sort the optimistic messages by read status

--- src/components/user/Messages.tsx
+++ src/components/user/Messages.tsx
@@ -61,85 +61,88 @@ export default function Messages({ messages, dbUnreadCount }: MessagesProps) {
 
   return (
     <>
-      {optimisticMessages.map((message) => {
-        const {
-          _id: id,
-          marketItem,
-          text,
-          sender,
-          phone,
-          createdAt,
-          read: isRead,
-        } = message;
-        return (
-          <li
-            key={id.toString()}
-            className="relative bg-white p-4 mob-lg:py-5 rounded-md border border-zinc-200 font-sans"
-          >
-            {!isRead && (
-              <div className="absolute top-2 right-2 text-base mob-lg:text-xs mob-lg:top-0.5 mob-lg:right-0.5 bg-amber-500 text-white px-2 py-1 rounded-md">
-                New
-              </div>
-            )}
-            <h2 className="text-xl mb-4">
-              <span className="font-bold">Market Item Inquiry:</span>{" "}
-              {marketItem.product.title} for{" "}
-              <span className="font-medium">
-                {formatCurrency(marketItem.price)}
-              </span>
-            </h2>
-            <p className="text-zinc-700 text-base">{text}</p>
-
-            <ul className="mt-4 flex flex-col gap-1">
-              <MessageInfo title="from">
-                <div className="flex items-center gap-2">
-                  {typeof sender.image === "string" && sender.image.length && (
-                    <Image
-                      src={sender.image}
-                      alt={`Image of ${sender.name}`}
-                      width={32}
-                      height={32}
-                      className="rounded-full"
-                    />
-                  )}
-                  <span>{sender.name}</span>
+      {optimisticMessages
+        .sort((a, b) => (a.read === b.read ? 0 : a.read ? 1 : -1))
+        .map((message) => {
+          const {
+            _id: id,
+            marketItem,
+            text,
+            sender,
+            phone,
+            createdAt,
+            read: isRead,
+          } = message;
+          return (
+            <li
+              key={id.toString()}
+              className="relative bg-white p-4 mob-lg:py-5 rounded-md border border-zinc-200 font-sans"
+            >
+              {!isRead && (
+                <div className="absolute top-2 right-2 text-base mob-lg:text-xs mob-lg:top-0.5 mob-lg:right-0.5 bg-amber-500 text-white px-2 py-1 rounded-md">
+                  New
                 </div>
-              </MessageInfo>
-              <MessageInfo title="Reply email">
-                {" "}
-                <a
-                  href={`mailto:${sender.email}`}
-                  className="text-blue-500 hover:underline focus:outline-none focus:underline"
-                >
-                  {sender.email}
-                </a>{" "}
-              </MessageInfo>
-              {phone && (
-                <MessageInfo title="Reply phone">
+              )}
+              <h2 className="text-xl mb-4">
+                <span className="font-bold">Market Item Inquiry:</span>{" "}
+                {marketItem.product.title} for{" "}
+                <span className="font-medium">
+                  {formatCurrency(marketItem.price)}
+                </span>
+              </h2>
+              <p className="text-zinc-700 text-base">{text}</p>
+
+              <ul className="mt-4 flex flex-col gap-1">
+                <MessageInfo title="from">
+                  <div className="flex items-center gap-2">
+                    {typeof sender.image === "string" &&
+                      sender.image.length && (
+                        <Image
+                          src={sender.image}
+                          alt={`Image of ${sender.name}`}
+                          width={32}
+                          height={32}
+                          className="rounded-full"
+                        />
+                      )}
+                    <span>{sender.name}</span>
+                  </div>
+                </MessageInfo>
+                <MessageInfo title="Reply email">
                   {" "}
                   <a
-                    href={`tel:${phone}`}
+                    href={`mailto:${sender.email}`}
                     className="text-blue-500 hover:underline focus:outline-none focus:underline"
                   >
-                    {phone}
-                  </a>
+                    {sender.email}
+                  </a>{" "}
                 </MessageInfo>
-              )}
-              <MessageInfo title="Received">
-                {format(new Date(createdAt), "MMM d, yyyy, h:mm a")}
-              </MessageInfo>
-            </ul>
-            <div className="flex gap-3 mt-4">
-              <ToggleMessageReadStatus
-                id={id}
-                onToggle={handleToggleReadStatus}
-                isRead={isRead}
-              />
-              <DeleteMessage message={message} />
-            </div>
-          </li>
-        );
-      })}
+                {phone && (
+                  <MessageInfo title="Reply phone">
+                    {" "}
+                    <a
+                      href={`tel:${phone}`}
+                      className="text-blue-500 hover:underline focus:outline-none focus:underline"
+                    >
+                      {phone}
+                    </a>
+                  </MessageInfo>
+                )}
+                <MessageInfo title="Received">
+                  {format(new Date(createdAt), "MMM d, yyyy, h:mm a")}
+                </MessageInfo>
+              </ul>
+              <div className="flex gap-3 mt-4">
+                <ToggleMessageReadStatus
+                  id={id}
+                  onToggle={handleToggleReadStatus}
+                  isRead={isRead}
+                />
+                <DeleteMessage message={message} />
+              </div>
+            </li>
+          );
+        })}
     </>
   );
 }
